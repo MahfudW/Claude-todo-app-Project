0c801bf266d748b4d41d05505c8d0efc2c743302
MahfudW <mahfud_80@yahoo.co.id>
2025-12-31 09:30:55 +0700

    Fix vite base URL and workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-to-gh-pages/',
+  base: '/Claude-todo-app-Project/', // ← Harus sesuai nama repo!
   plugins: [react()],
 })
